b0f390feba02a63f93a18a77d4aded7fbc4cb48a
danivst <d.stan4eva07@gmail.com>
2026-04-25 22:34:05 +0300

    Fixed env in vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-import { BASE_API_URL } from "./src/utils/env";
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  logLevel: "info",
-  server: {
-    port: 5173,
-    host: true,
-    proxy: {
-      "/api": {
-        target: BASE_API_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    logLevel: "info",
+    server: {
+      port: 5173,
+      host: true,
+      proxy: {
+        "/api": {
+          // Access the variable from the 'env' object created above
+          target: env.BASE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path,
+        },
       },
     },
-  },
+  };
 });
